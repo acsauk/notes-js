e2684617ba8e0ba38e943c63165ffbfb1bc7d3ab
acsauk <acsauk@gmail.com>
2016-08-18 10:59:28 +0100

    removes uneeded files

--- spec/note_spec.js
+++
@@ -1,9 +0,0 @@
-var Note = require("../src/note-model").Note;
-var isTrue = require("../src/asserts").isTrue;
-
-function testNotesTextInstantiation() {
-  var note = new Note("My favourite language is JavaScript");
-  isTrue(note.text == "My favourite language is JavaScript");
-}
-
-testNotesTextInstantiation();
